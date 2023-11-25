3969489c97aa42eae805bda8971cb2b3624e5778
Pau15122 <77733880+vandangnhathung@users.noreply.github.com>
2023-11-25 20:00:29 +0700

    fix: update directory

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,25 +1,27 @@
 document.querySelectorAll(".um-parallax").forEach((el, i) => {
 
-    window.addEventListener("scroll", () => {
-        el.style.transform = `translateY(${window.scrollY * (i + 1) * 0.01}%`;
+    setTimeout(() => {
+        window.addEventListener("scroll", () => {
+            el.style.transform = `translateY(${window.scrollY * (i + 1) * 0.01}%`;
 
-        const title = document.querySelector(".um-main-title");
+            const title = document.querySelector(".um-main-title");
 
-        const rectTitle = title.getBoundingClientRect();
-        const titleOffsetTop = rectTitle.top + window.scrollY + rectTitle.height;
-        const titleOffsetRight = rectTitle.right - rectTitle.left;
-        // console.log(titleOffsetTop, rectTitle.left, rectTitle.right)
+            const rectTitle = title.getBoundingClientRect();
+            const titleOffsetTop = rectTitle.top + window.scrollY + rectTitle.height;
+            const titleOffsetRight = rectTitle.right - rectTitle.left;
+            // console.log(titleOffsetTop, rectTitle.left, rectTitle.right)
 
-        const rectEl = el.getBoundingClientRect();
-        const elOffsetTop = rectEl.top + window.scrollY;
+            const rectEl = el.getBoundingClientRect();
+            const elOffsetTop = rectEl.top + window.scrollY;
 
 
-        if(titleOffsetTop >= elOffsetTop && rectTitle.left <= rectEl.right && rectEl.right <= rectTitle.right){
-            // console.log("elOffsetTop: ", elOffsetTop, "rectEl.left: ", rectEl.left, "rectEl.right: ", rectEl.right, el, true);
-        }else{
-            // console.log("elOffsetTop: ", elOffsetTop, "rectEl.left: ", rectEl.left, "rectEl.right: ", rectEl.right, el, false);
-        }
-    })
+            if (titleOffsetTop >= elOffsetTop && rectTitle.left <= rectEl.right && rectEl.right <= rectTitle.right) {
+                // console.log("elOffsetTop: ", elOffsetTop, "rectEl.left: ", rectEl.left, "rectEl.right: ", rectEl.right, el, true);
+            } else {
+                // console.log("elOffsetTop: ", elOffsetTop, "rectEl.left: ", rectEl.left, "rectEl.right: ", rectEl.right, el, false);
+            }
+        })
+    }, 400)
 })
 
 
@@ -36,20 +38,23 @@ document.querySelectorAll(".um-slide-img-wrapper").forEach((el, i) => {
 
         // min-max scrolling y axios distance
         const minYDistance = slideSection.getBoundingClientRect().top + window.scrollY;
-        // const maxYDistance = umMainSlide.getBoundingClientRect().bottom + window.scrollY;
+        const maxYDistance = slideSection.getBoundingClientRect().bottom + window.scrollY;
         // const activeArea = maxYDistance - minYDistance;
+
+        console.log("maxYDistance: ", maxYDistance)
 
         // normalize method -> convert custom value to [0,1]
         const scrollingActiveArea = window.scrollY - minYDistance;
         const convertValue = (scrollingActiveArea / hiddenArea); // [0,1]
 
+        console.log("convertValue: ", scrollingActiveArea)
         // area is active => scroll into area | area is deactivated => leave off area
-        if(scrollingActiveArea >= 0 && scrollingActiveArea <= slideSection.offsetHeight && convertValue <= 1){
+        if (scrollingActiveArea >= 0 && scrollingActiveArea <= slideSection.offsetHeight && convertValue <= 1) {
             const realValue = Math.floor(convertValue * 100);
 
-            if(isOdd){
+            if (isOdd) {
                 el.style.transform = `translateX(${-realValue}%)`;
-            }else{
+            } else {
                 el.style.transform = `translateX(${-100 + realValue}%)`;
             }
         }
